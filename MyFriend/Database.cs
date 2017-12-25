0ace2f4ec8feafedcef83c79053b6d2da03e928c
trikstor <azpk360@yandex.ru>
2017-12-25 22:57:57 +0500

    init

---
+++ MyFriend/Database.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SQLite;
+
+namespace MyFriend
+{
+    public class Database
+    {
+        private SQLiteConnection connection { get; set; }
+
+        public Database Connect(string databaseName)
+        {
+            connection = new SQLiteConnection($"Data Source={AppDomain.CurrentDomain.BaseDirectory}{databaseName}");
+            return this;
+        }
+
+        public IEnumerable<SQLiteDataReader> Select(string request)
+        {
+            using (var cmd = new SQLiteCommand(request, connection))
+            {
+                using (var reader = cmd.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        yield return reader;
+                    }
+                }
+            }
+            connection.Close();
+        }
+        
+        public bool TryExecuteCmd(string request)
+        {
+            using (var cmd = new SQLiteCommand(request))
+            {
+                try
+                {
+                    cmd.ExecuteNonQuery();
+                }
+                catch (Exception ex)
+                {
+                    connection.Close();
+                    return false;
+                }
+            }
+            connection.Close();
+            return true;
+        }
+
+        public void ConnectionClose()
+        {
+            connection.Close();
+        }
+    }
+}
