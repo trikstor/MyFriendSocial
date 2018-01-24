8f01e7958fa30cd52218b2f6dffcaf12274b8386
trikstor <azpk360@yandex.ru>
2018-01-24 18:23:30 +0500

    Рабочая версия с багами.

---
+++ Backup1/MyFriend/Auth/FormValidator.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using EmailValidation;
+
+namespace MyFriend.Auth
+{
+    public class FormValidator : IFormValidator
+    {       
+        public bool FieldIsValid(string field)
+        {
+            var specialCharacters = new[]
+            {
+                '\"',
+                '\'',
+                '\\',
+                '{',
+                '}'
+            };
+            return field.Length > 2 && field.Length < 15 && field.NotContains(specialCharacters);
+        }
+
+        public bool EmailIsValid(string email)
+        {
+            var tt = EmailValidator.Validate(email);
+            Console.WriteLine(tt);
+            return tt;
+        }
+    }
+}
